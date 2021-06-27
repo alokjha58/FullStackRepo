f80162d4988106ef76e16d585ffdaf21f2b7e1bc
DEVELOPER <DEVELOPER@VIKASH>
2021-06-27 22:21:15 +0530

    testing code...

---
+++ Proj1/src/com/nt/comp/MainApplication.java
@@ -0,0 +1,12 @@
+package com.nt.comp;
+
+public class MainApplication {
+
+	public static void main(String[] args) {
+A a1=new A();
+B b1=new B();
+System.out.println("sum is ::"+a1.sum(10, 20));
+System.out.println("sub is ::"+b1.sub(15, 10));
+}
+
+}
